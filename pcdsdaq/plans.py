ad170895dfb21398fa679263757cbd9335f77491
teddyrendahl <trendahl@slac.stanford.edu>
2018-09-24 16:08:11 -0700

    ENH: Include configuration of DAQ latency compensation

--- pcdsdaq/plans.py
+++ pcdsdaq/plans.py
@@ -5,7 +5,7 @@ beamline equipment relevant to run control
 import bluesky.plan_stubs as bps
 
 
-def sequencer_mode(daq, sequencer, iterations):
+def sequencer_mode(daq, sequencer, iterations, sequence_wait=250):
     """
     Configure the DAQ to be controlled by the EventSequencer
     """
@@ -13,6 +13,8 @@ def sequencer_mode(daq, sequencer, iterations):
     yield from bps.configure(daq, events=0)
     # Configure the EventSequencer to run for a specified number of sequences
     yield from bps.configure(sequencer, play_mode=1, rep_count=iterations)
+    # Configure the EventSequencer to wait for the DAQ to arm itself
+    sequencer.DEFAULT_SLEEP = sequence_wait
     # TODO: It would be nice to add additional checks here to ensure that our
     # configuration is reasonable
     # * Is the DAQ configured to readout on an event code?
