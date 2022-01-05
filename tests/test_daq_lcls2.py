1648d066c7e0dbe810aa00f892047e265fd831a2
Zachary Lentz <zlentz@slac.stanford.edu>
2022-01-05 15:14:24 -0800

    TST: begin writing unit tests

--- tests/test_daq_lcls2.py
+++ tests/test_daq_lcls2.py
@@ -1,10 +1,17 @@
+import logging
+
 import pytest
+from ophyd.positioner import SoftPositioner
+from ophyd.signal import Signal
+from psdaq.control.ControlDef import ControlDef
 
 from pcdsdaq.daq import DaqLCLS2
 
+logger = logging.getLogger(__name__)
+
 
 @pytest.fixture(scope='function')
-def daq_lcls2(RE):
+def daq_lcls2(RE) -> DaqLCLS2:
     return DaqLCLS2(
         platform=0,
         host='tst',
@@ -15,14 +22,66 @@ def daq_lcls2(RE):
     )
 
 
-def test_state():
-    # Should work at any point
-    1/0
+def test_state(daq_lcls2: DaqLCLS2):
+    """Check that the state attribute reflects the DAQ state."""
+    logger.debug('test_state')
+    for state in ControlDef.states:
+        daq_lcls2._control.sim_set_states(0, state)
+        assert daq_lcls2.state == state
 
 
-def test_preconfig():
-    # Should work at any point
-    1/0
+def test_preconfig(daq_lcls2: DaqLCLS2):
+    """
+    Check that preconfig has the following behavior:
+    - Writes to cfg signals
+    - Checks types, raising TypeError if needed
+    - Has no change for unpassed kwargs
+    - Reverts "None" kwargs to the default
+    """
+    logger.debug('test_preconfig')
+
+    def test_one(keyword, good_values, bad_values):
+        if keyword == 'motors':
+            # Backcompat alias
+            sig = daq_lcls2.controls_cfg
+        else:
+            sig = getattr(daq_lcls2, keyword + '_cfg')
+        orig = sig.get()
+        for value in good_values:
+            daq_lcls2.preconfig(*{keyword: value})
+            assert sig.get() == value
+        some_value = sig.get()
+        daq_lcls2.preconfig(show_queued_config=False)
+        assert sig.get() == some_value
+        for value in bad_values:
+            with pytest.raises(TypeError):
+                daq_lcls2.preconfig(*{keyword: value})
+        daq_lcls2.preconfig(*{keyword: None})
+        assert sig.get() == orig
+
+    test_one('events', (1, 10, 100), (45.3, 'peanuts', object()))
+    test_one('duration', (1, 2.3, 100), ('walnuts', object()))
+    test_one('record', (True, False), (1, 0, object()))
+
+    good_controls = dict(
+        sig=Signal(name='sig'),
+        mot=SoftPositioner(name='mot'),
+    )
+    for kw in ('controls', 'motors'):
+        test_one(
+            kw,
+            (good_controls, list(good_controls.values())),
+            ('text', 0, dict(bad=object()), [object()], Signal(name='sig')),
+        )
+
+    test_one('begin_timeout', (1, 2.3, 100), ('cashews', object()))
+    test_one('begin_sleep', (1, 2.3, 100), ('pistachios', object()))
+    test_one('group_mask', (1, 10, 100), (23.4, 'coconuts', object()))
+    test_one('detname', ('epix', 'wave8'), (1, 2.3, object()))
+    test_one('scantype', ('cool', 'data'), (1, 2.4, object()))
+    test_one('serial_number', ('213', 'AV34'), (1, 2.5, object()))
+    test_one('alg_name', ('math', 'calc'), (1, 2.6, object()))
+    test_one('alg_version', ([1, 2, 3], [2, 3, 4]), (1, 2.6, 'n', object()))
 
 
 def test_record():
