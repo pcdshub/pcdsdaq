cb7a696604d5b3c5791d9b8c87a42201b9adc48b
Zachary Lentz <zlentz@slac.stanford.edu>
2022-01-05 14:33:41 -0800

    TST: rearrange lcls2 tests

--- tests/test_daq_lcls2.py
+++ tests/test_daq_lcls2.py
@@ -15,18 +15,8 @@ def daq_lcls2(RE):
     )
 
 
-def test_configured():
-    # Test this after configure
-    1/0
-
-
 def test_default_config():
     # Should work at any point
-    1/0
-
-
-def test_config():
-    # Test this after configure
     1/0
 
 
@@ -35,62 +25,22 @@ def test_state():
     1/0
 
 
-def test_wait():
-    # Test this after kickoff
-    1/0
-
-
-def test_begin():
-    # Test this after wait
-    1/0
-
-
-def test_begin_infinite():
-    # Test this after stop
-    1/0
-
-
-def test_stop():
-    # Test this after begin
-    1/0
-
-
-def test_end_run():
-    # Test this after stop
-    1/0
-
-
-def test_trigger():
-    # Test this after kickoff
-    1/0
-
-
-def test_read():
-    # Test this after begin_infinite
-    1/0
-
-
-def test_kickoff():
-    # Test this after configure
-    1/0
-
-
-def test_complete():
-    # Test this after collect
-    1/0
-
-
-def test_collect():
-    # Test this after complete
-    1/0
-
-
-def test_describe_collect():
-    # Test this after collect
-    1/0
-
-
 def test_preconfig():
+    # Should work at any point
+    1/0
+
+
+def test_record():
+    # Should work at any point
+    1/0
+
+
+def test_run_number():
+    # Should work at any point
+    1/0
+
+
+def test_stage_unstage():
     # Should work at any point
     1/0
 
@@ -105,13 +55,53 @@ def test_config_info():
     1/0
 
 
-def test_record():
-    # Should work at any point
+def test_configured():
+    # Test this after configure
     1/0
 
 
-def test_stage_unstage():
-    # Should work at any point
+def test_config():
+    # Test this after configure
+    1/0
+
+
+def test_kickoff():
+    # Test this after configure
+    1/0
+
+
+def test_wait():
+    # Test this after kickoff
+    1/0
+
+
+def test_trigger():
+    # Test this after kickoff
+    1/0
+
+
+def test_begin():
+    # Test this after wait
+    1/0
+
+
+def test_stop():
+    # Test this after begin
+    1/0
+
+
+def test_begin_infinite():
+    # Test this after stop
+    1/0
+
+
+def test_end_run():
+    # Test this after stop
+    1/0
+
+
+def test_read():
+    # Test this after begin_infinite
     1/0
 
 
@@ -120,8 +110,18 @@ def test_pause_resume():
     1/0
 
 
-def test_run_number():
-    # Should work at any point
+def test_collect():
+    # Test this after kickoff
+    1/0
+
+
+def test_complete():
+    # Test this after collect
+    1/0
+
+
+def test_describe_collect():
+    # Test this after collect
     1/0
 
 
